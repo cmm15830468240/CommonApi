e6e2185b362a2a48570b539d86510385a1a84ff3
cmm <chenmm@17zhuange.com>
2016-05-11 11:44:52 +0800

    1

---
+++ Architecture/ApiBase/BaseHeadEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architecture.ApiBase
+{
+    /// <summary>
+    /// 基础请求信息
+    /// </summary>
+    public abstract class BaseHeadEntity
+    {
+        /// <summary>
+        /// 授权字符
+        /// </summary>
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public int LoginId { get; set; }
+    }
+}
+
---
+++ Architecture/ApiBase/ResponseHeadEntity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Architecture.ApiBase
+{
+    /// <summary>
+    /// 响应头部描述信息
+    /// </summary>
+    public abstract class ResponseHeadEntity
+    {
+        /// <summary>
+        /// 默认构造函数
+        /// </summary>
+        public ResponseHeadEntity()
+        {
+            rspCode = string.Empty;
+            rspDesc = string.Empty;
+        }
+
+        /// <summary>
+        /// 响应标识
+        /// </summary>
+        public string rspCode { get; set; }
+
+        /// <summary>
+        /// 响应信息描述
+        /// </summary>
+        public string rspDesc { get; set; }
+    }
+}
